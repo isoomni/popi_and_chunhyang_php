d20d06101053cb67fa5c4378073665f5ba856e35
root <root@ip-172-31-14-71.ap-northeast-2.compute.internal>
2021-12-04 06:19:39 +0000

    login change

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 
 $conn = new mysqli("13.124.144.102","root","Ga1234!!","pac");
 
-mysqli_query($conn,'utf8');
+mysqli_query($conn,'SET NAMES utf8');
 
 $uid = $_POST['user_nickname'];
 $pw = $_POST['user_pw'];
@@ -15,15 +15,16 @@ $res = $conn->query($sql);
 $row = mysqli_fetch_array($res);
 
 if($res -> num_rows >0){
-    $_SESSION['user_nickname'] = $uid;
-
-    if(isset($_SESSION['user_nickname'])){
+    $_SESSION['userNickname'] = $uid;
+    $_SESSION['password'] = $pw;
+    
+    if(isset($_SESSION['userNickname'])&&isset($_SESSION['password'])){
         echo "<script>location.href='http://13.124.144.102/home.html'</script>";
     } else{
-        echo "<script> alert('로그인 실패!');</script>";
-    }} else{
-        echo "<script> alert('로그인 실패!');</script>";
+        echo "<script> alert('로그인 실패!1');</script>";
     }
+} else{
+    echo "<script> alert('로그인 실패!2');</script>";
 }
 
 ?>
